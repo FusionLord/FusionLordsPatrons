6898b9526b2dd4304a316571a19611aec265ef2b
FusionLord <FusionLord@live.com>
2016-07-19 01:20:07 -0700

    Initial commit

---
+++ src/main/java/net/fusionlord/patrons/FusionLordsPatrons.java
@@ -0,0 +1,9 @@
+package net.fusionlord.patrons;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = FusionLordsPatrons.ID, name = FusionLordsPatrons.NAME)
+public class FusionLordsPatrons {
+    static final String ID = "fusionlordspatrons";
+    static final String NAME = "FusionLord's Patrons";
+}
